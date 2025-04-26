b3931a3976580888617e28fd51cb62ae697f8432
iosアカデミア <yutamac2512@icloud.com>
2025-04-26 17:13:51 +0900

    コミット

---
+++ GithubPractice/GithubPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubPractice
+//
+//  Created by Engineer MacBook Air on 2025/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
